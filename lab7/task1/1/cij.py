f5094390b7d156589a7d9dd9c4c947c5bec9cb8e
altyyyn_aaai <122877634+Dokaaa@users.noreply.github.com>
2024-03-24 16:28:58 +0600

    Add files via upload

---
+++ lab7/task1/1/cij.py
@@ -0,0 +1,5 @@
+total_sum = 0
+for i in range(100):
+    num = int(input())
+    total_sum += num
+print(total_sum)
---
+++ lab7/task1/1/cim.py
@@ -0,0 +1,8 @@
+N = int(input())
+z = 0
+
+for i in range(N):
+    num = int(input())
+    if num == 0:
+        z += 1
+print(z)
